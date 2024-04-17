4741910080aa3fee639da8aa8606328242e128f8
DHANUSHAN <dhanushan.info@gmail.com>
2024-04-18 00:06:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    item.classList.toggle("collapsible--expanded");
+  })
+);
+
